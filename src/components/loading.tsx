215e0dd3696430f70755c014679da4375e028208
boyninja15 <flappyfloorg@gmail.com>
2025-10-08 16:44:12 -0500

    Fixed loading indicator

--- src/components/loading.tsx
+++ src/components/loading.tsx
@@ -2,6 +2,8 @@ import "@/styles/loader.css"
 
 export default function Loading() {
     return (
-        <span className="loader top-1/2 left-1/2 -translate-1/2 absolute" />
+        <div className="w-full h-screen">
+            <span className="loader top-1/2 left-1/2 -translate-1/2 absolute" />
+        </div>
     )
 }
